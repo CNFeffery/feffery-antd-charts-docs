06cb1fcf9fd56ea986909ae71cb8a301f8b4a19b
CNFeffery <fefferypzy@gmail.com>
2025-01-21 09:40:45 +0800

    doc: AntdColumn文档页新增示例“开启缩略轴”

--- views/AntdColumn/demos/__init__.py
+++ views/AntdColumn/demos/__init__.py
@@ -3,6 +3,7 @@ from dash.dependencies import Component
 
 from . import (
     basic_usage,  # noqa: F401
+    slider,  # noqa: F401
 )
 from components import demos_render
 
@@ -17,6 +18,11 @@ def demos_config() -> list:
             'path': 'basic_usage',
             'title': t('基础使用'),
             'description': '最基础的柱状图。',
+        },
+        {
+            'path': 'slider',
+            'title': '开启缩略轴',
+            'description': '通过参数`slider`开启缩略轴相关功能。',
         },
     ]
 
