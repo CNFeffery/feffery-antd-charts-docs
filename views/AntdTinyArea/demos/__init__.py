76dbe1b41399be82ae4a558fb2e955cf94784721
CNFeffery <fefferypzy@gmail.com>
2025-01-23 21:24:05 +0800

    doc: AntdTinyArea文档页新增示例“添加标注线”

--- views/AntdTinyArea/demos/__init__.py
+++ views/AntdTinyArea/demos/__init__.py
@@ -4,6 +4,7 @@ from dash.dependencies import Component
 from . import (
     basic_usage,  # noqa: F401
     pattern,  # noqa: F401
+    line_annotation,  # noqa: F401
 )
 from components import demos_render
 
@@ -23,6 +24,11 @@ def demos_config() -> list:
             'path': 'pattern',
             'title': '填充图案',
             'description': '通过参数`pattern`设置填充图案。',
+        },
+        {
+            'path': 'line_annotation',
+            'title': '添加标注线',
+            'description': '通过参数`annotations`添加辅助标注线。',
         },
     ]
 
