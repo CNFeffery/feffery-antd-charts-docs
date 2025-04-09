79cb3468838ce78a392da88695d8574f8ac5e1d8
CNFeffery <fefferypzy@gmail.com>
2025-04-09 15:20:49 +0800

    doc: AntdWaterfall文档页新增示例“自定义总计值显示”

--- views/AntdWaterfall/demos/__init__.py
+++ views/AntdWaterfall/demos/__init__.py
@@ -3,6 +3,7 @@ from dash.dependencies import Component
 
 from . import (
     basic_usage,  # noqa: F401
+    custom_total,  # noqa: F401
 )
 from components import demos_render
 
@@ -17,6 +18,11 @@ def demos_config() -> list:
             'path': 'basic_usage',
             'title': t('基础使用'),
             'description': '最基础的瀑布图。',
+        },
+        {
+            'path': 'custom_total',
+            'title': '自定义总计值显示',
+            'description': '通过参数`total`自定义总计值显示。',
         },
     ]
 
