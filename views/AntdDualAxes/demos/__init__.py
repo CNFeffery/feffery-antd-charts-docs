b70338adc296384fd67a0cfe9142ecc588a4f25e
CNFeffery <fefferypzy@gmail.com>
2025-04-22 17:34:27 +0800

    doc: AntdDualAxes文档页新增示例“多折线+柱体混合双轴图”

--- views/AntdDualAxes/demos/__init__.py
+++ views/AntdDualAxes/demos/__init__.py
@@ -6,6 +6,7 @@ from . import (
     line_and_column,  # noqa: F401
     dual_line_style,  # noqa: F401
     dual_line_step,  # noqa: F401
+    multi_line_and_column,  # noqa: F401
 )
 from components import demos_render
 
@@ -35,6 +36,11 @@ def demos_config() -> list:
             'path': 'dual_line_step',
             'title': '阶梯型折线双轴图',
             'description': '针对折线双轴图，自定义折线为阶梯型。',
+        },
+        {
+            'path': 'multi_line_and_column',
+            'title': '多折线+柱体混合双轴图',
+            'description': '多折线+柱体混合双轴图。',
         },
     ]
 
