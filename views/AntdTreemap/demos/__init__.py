0cef5c0ee13066811b551e28b29f7f9e3b6cbea0
CNFeffery <fefferypzy@gmail.com>
2025-04-24 16:03:26 +0800

    doc: AntdTreemap文档页新增示例'嵌套矩形树图

--- views/AntdTreemap/demos/__init__.py
+++ views/AntdTreemap/demos/__init__.py
@@ -3,6 +3,7 @@ from dash.dependencies import Component
 
 from . import (
     basic_usage,  # noqa: F401
+    nested,  # noqa: F401
 )
 from components import demos_render
 
@@ -17,6 +18,11 @@ def demos_config() -> list:
             'path': 'basic_usage',
             'title': t('基础使用'),
             'description': '最基础的矩形树图。',
+        },
+        {
+            'path': 'nested',
+            'title': '嵌套矩形树图',
+            'description': '嵌套形式展示矩形树图。',
         },
     ]
 
